74b9b64c83172d5c6c96f7aa1a30360024e29db5
Vyto4ka <qqwpo@yandex.ru>
2025-05-22 16:51:53 +0300

    fix

--- modules/winapps.nix
+++ modules/winapps.nix
@@ -1,13 +1,13 @@
 { config, pkgs, lib, inputs, ... }:
 
 let
-  target = pkgs.system;                            # e.g. "x86_64-linux"
-  wa     = inputs.winapps.packages.${target};     # пакеты из flake winapps
+  target = pkgs.system;
+  wa     = inputs.winapps.packages.${target};
 in {
   nix.settings = {
     substituters        = [ "https://winapps.cachix.org/" ];
     trusted-public-keys = [ "winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g=" ];
-    trusted-users       = [ "vyto4ka" ];            # ваше ЛК пользователя
+    trusted-users       = [ "vyto4ka" ];
   };
 
   environment.systemPackages = [
