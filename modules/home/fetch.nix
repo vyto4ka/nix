077f9c20385504f194b5ca3159a62d96dc9742e8
vyto4ka <QQWPO@yandex.ru>
2025-06-23 16:15:45 +0300

    remove some shit

--- modules/home/fetch.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-  home.file = lib.mkForce {
-    ".config/neofetch/config.conf".source = ../../dotfiles/neofetch/config.conf;
-    ".config/fastfetch/config.jsonc".source = ../../dotfiles/fastfetch/config.jsonc;
-  };
-}
--- modules/home/kitty.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-  home.file = lib.mkForce {
-    ".config/kitty/kitty.conf".source         = ../../dotfiles/kitty/kitty.conf;
-    ".config/kitty/current-theme.conf".source = ../../dotfiles/kitty/current-theme.conf;
-  };
-}
